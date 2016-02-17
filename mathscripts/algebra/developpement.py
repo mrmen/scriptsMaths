a09e3e168430f003beecadd0e8a5e8070f93db90
Thomas Etcheverria <tetcheve@gmail.com>
2016-02-17 21:34:33 +0100

    double d : initial

--- mathscripts/algebra/developpement.py
+++ mathscripts/algebra/developpement.py
@@ -2,7 +2,7 @@ __author__ = 'mrmen'
 
 from TemplateExercice import *
 import random
-from sympy import abc, simplify
+from sympy import expand
 
 
 class Developpement(TemplateExercice):
@@ -12,7 +12,14 @@ class Developpement(TemplateExercice):
         self.max = 10
         self.nb = ["x", "y", ""]
         self.sign = ["\\times"]
+        self.additive = ["+", "-"]
         self.generate()
+
+    def random_except_zero(self):
+        x = 0
+        while (x==0):
+            x = random.randint(self.min, self.max)
+        return x
 
     def generate(self):
         """generate enonce and solution according to self.nbQuestion and self.nbExercice from parent class"""
@@ -24,39 +31,48 @@ class Developpement(TemplateExercice):
                 nbnumbers = 4
                 values = []
                 for _ in range(nbnumbers):
-                    value = random.randint(self.min, self.max)
-                    values.append([value, self.nb[random.randint(0,len(self.nb)-1)])
-                #
+                    value = self.random_except_zero()
+                    if (_==0) or (_==2):
+                        values.append([value, self.nb[random.randint(0,len(self.nb)-1)], self.additive[random.randint(0,1)]])
+                    else:
+                        values.append([value, self.nb[random.randint(0,len(self.nb)-1)]])
                 tempenonce.append(self.format_enonce(values))
                 tempsolution.append(self.format_solution(values))
             self.listeEnonce.append(tempenonce)
             self.listeSolution.append(tempsolution)
 
-
     def format_enonce(self, liste):
         string = []
         for elt in liste:
             fact = elt[0]
+            if (fact < 0):
+                fact = "(" + str(fact) + ")"
             variable = elt[1]
-            if variable == "":
-                variable = 1              
-            string.append(str(fact) + "\\times" + str(variable))
-         return self.concat_enonce(string)
+            if (len(elt)==3):
+                variable += elt[2]
+            if (variable == "+") or (variable == "-") or (variable==""):
+                string.append(str(fact) + variable)
+            else:
+                string.append(str(fact) + " \\times " + str(variable))
+        return self.concat_enonce(string)
 
-    def concat_enonce(liste):
+    def concat_enonce(self, liste):
         count = 0
         string = "("
-        for _ in liste:
-            string += _
+        for elt in liste:
+            string += elt+" "
             count +=1
             if count == 2:
-                string+=")("
+                string+=")\\times ("
         string+=")"
         return "$" + string + "$"
 
     def format_solution(self, liste):
-        return "None"
+        calc_initial = self.format_enonce(liste).replace("$", "")
+        calc = calc_initial.replace("\\times", "*")
+        return "$"+str(calc_initial)+" = "+str(expand(calc))+"$"
+
 
 if __name__ == "__main__":
-    app = AdditionDecimaux()
+    app = Developpement()
     app.display()
