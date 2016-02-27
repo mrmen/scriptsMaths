550950946647dfea3c2f500723e9e01920a110ac
Thomas Etcheverria <tetcheve@gmail.com>
2016-02-27 22:36:42 +0100

    fix et ajout mult par 0.1

--- mathscripts/algebra/multiplication-01.py
+++ mathscripts/algebra/multiplication-01.py
@@ -4,7 +4,7 @@ from TemplateExercice import *
 import random
 
 
-class Mult10(TemplateExercice):
+class Mult01(TemplateExercice):
     def __init__(self):
         TemplateExercice.__init__(self)
         self.min = 1
@@ -22,7 +22,7 @@ class Mult10(TemplateExercice):
                 first = random.randint(1,5)
                 secondprime = random.randint(self.min, self.max)
                 second = float(secondprime)/10**first
-                third = random.randint(1,4)
+                third = random.randint(-4,-1)
                 tempenonce.append("$%s \\times %s$" % (second, 10**third))
                 tempsolution.append("$%s \\times %s = $\\vspace*{3cm}" % (second, 10**third))
             self.listeEnonce.append(tempenonce)
@@ -30,5 +30,5 @@ class Mult10(TemplateExercice):
 
 
 if __name__ == "__main__":
-    app = Mult10()
+    app = Mult01()
     app.display()
