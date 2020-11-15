dffd921be9efdce088f0d2ba474199da558dec4b
Thomas Etcheverria <mrmen@users.noreply.github.com>
2020-11-15 21:29:24 +0100

    added support for ipad pythonista clipboard

--- mathscripts/algebra/TemplateExercice.py
+++ mathscripts/algebra/TemplateExercice.py
@@ -1,40 +1,52 @@
 __author__ = 'mrmen'
 
+# pour l'ipad
+import clipboard
+
+clip = ""
+clipboard.set(clip)
+
+def mrmen_print(string):
+	global clip
+	print(string)
+	clip = clip + "\n" + string
+	clipboard.set(clip)
+
 
 class TemplateExercice:
     def __init__(self):
         """create all needed parts"""
         self.listeEnonce = []
         self.listeSolution = []
-        self.nbExercice = 1
+        self.nbExercice = 5
         self.nbQuestion = 10
         self.COLONNE=1
         self.package = ["amsmath", "amssymb", "inputenc", "fontenc", "geometry", "xlop", "multicol", "xcolor"]
 
     @staticmethod
     def format_exercice(exercice, exercise_type):
-        """print an exercise with its question"""
-        print("\\begin{" + exercise_type + "}\\ ")
-        print("")
-        print("\\begin{enumerate}")
+        """mrmen_print an exercise with its question"""
+        mrmen_print("\\begin{" + exercise_type + "}\\ ")
+        mrmen_print("")
+        mrmen_print("\\begin{enumerate}")
         for question in exercice:
-            print("\\item " + question)
-        print("\\end{enumerate}")
-        print("\\end{" + exercise_type + "}")
+            mrmen_print("\\item " + question)
+        mrmen_print("\\end{enumerate}")
+        mrmen_print("\\end{" + exercise_type + "}")
 
-    def print_enonce(self):
-        """print enonce according to listeEnonce"""
+    def mrmen_print_enonce(self):
+        """mrmen_print enonce according to listeEnonce"""
         for exercice in self.listeEnonce:
             self.format_exercice(exercice, "exercice")
 
-    def print_solution(self):
-        """print solution according to listeSolution"""
+    def mrmen_print_solution(self):
+        """mrmen_print solution according to listeSolution"""
         for solution in self.listeSolution:
             self.format_exercice(solution, "solution")
 
-    def print_preamble(self):
-        """print preamble according to self.package"""
-        print("\\documentclass[12pt,french]{article}")
+    def mrmen_print_preamble(self):
+        """mrmen_print preamble according to self.package"""
+        mrmen_print("\\documentclass[12pt,french]{article}")
         for package in self.package:
             line = "\\usepackage"
             if package == "geometry":
@@ -44,29 +56,29 @@ class TemplateExercice:
             elif package == "fontenc":
                 line += "[T1]"
             line += "{" + package + "}"
-            print(line)
-        print("\\newtheorem{exercice}{Exercice}")
-        print("\\newtheorem{solution}{Solution de l'exercice }")
-        print("\\begin{document}")
+            mrmen_print(line)
+        mrmen_print("\\newtheorem{exercice}{Exercice}")
+        mrmen_print("\\newtheorem{solution}{Solution de l'exercice }")
+        mrmen_print("\\begin{document}")
 
     @staticmethod
-    def print_postamble():
-        """print postamble ie only end document"""
-        print("\\end{document}")
+    def mrmen_print_postamble():
+        """mrmen_print postamble ie only end document"""
+        mrmen_print("\\end{document}")
 
     def display(self):
         """display the whole exercise according to previous functions"""
-        self.print_preamble()
+        self.mrmen_print_preamble()
         if self.COLONNE!=0:
-            print("\\begin{multicols}{2}")
+            mrmen_print("\\begin{multicols}{2}")
         # TODO
-        self.print_enonce()
+        self.mrmen_print_enonce()
         if self.COLONNE!=0:
-            print("\\end{multicols}")
+            mrmen_print("\\end{multicols}")
         # TODO fix this part
-        print("\\newpage")
-        print("\\begin{multicols}{2}")
+        mrmen_print("\\newpage")
+        mrmen_print("\\begin{multicols}{2}")
         # TODO
-        self.print_solution()
-        print("\\end{multicols}")
-        self.print_postamble()
+        self.mrmen_print_solution()
+        mrmen_print("\\end{multicols}")
+        self.mrmen_print_postamble()
